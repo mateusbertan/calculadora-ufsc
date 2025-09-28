988ab17b634b50be08a4164ddc66886e5099ec42
Mateus Bertan <contato@mateusbertan.com>
2025-09-28 19:03:24 -0300

    Melhor legibilidade do código

--- js/notafinal.js
+++ js/notafinal.js
@@ -1,63 +1,63 @@
 const cursosPath = '/assets/cursos.json';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const campusSelect = document.getElementById('campus');
-    const cursoSelect = document.getElementById('curso');
-    const elementosPeso = document.getElementsByClassName('peso');
+  const campusSelect = document.getElementById('campus');
+  const cursoSelect = document.getElementById('curso');
+  const elementosPeso = document.getElementsByClassName('peso');
 
-    fetch(cursosPath)
+  fetch(cursosPath)
     .then((response) => response.json())
     .then((json) => {
-        const cursos = json;
+      const cursos = json;
+
+      cursos.forEach(curso => {
+        if (curso.campus === campusSelect.value) {
+          atualizarCursos(curso);
+        };
+      });
+
+      const pesosPrimeiroCurso = cursos[0].pesos;
+
+      for (i = 0; i < elementosPeso.length; i++) {
+        const disciplina = elementosPeso[i].className.slice(5, 8);
+        const peso = pesosPrimeiroCurso[disciplina];
+        elementosPeso[i].innerHTML = `<b>${String(peso).replace('.', ',')}</b>`;
+      };
+
+      campusSelect.addEventListener("change", () => {
+        cursoSelect.innerHTML = '';
 
         cursos.forEach(curso => {
-            if (curso.campus === campusSelect.value) {
-                atualizarCursos(curso);
-            };
+          if (curso.campus === campusSelect.value) {
+            atualizarCursos(curso);
+          };
         });
-        
-        const pesosPrimeiroCurso = cursos[0].pesos;
+
+        atualizarPesos();
+      });
+
+      cursoSelect.addEventListener("change", () => {
+        atualizarPesos();
+      });
+
+      function atualizarCursos(curso) {
+        const option = document.createElement('option');
+        option.value = curso.curso;
+        option.textContent = curso.nome;
+        cursoSelect.appendChild(option);
+      };
+
+      function atualizarPesos() {
+        const numeroCurso = cursoSelect.value;
+        const arrayCurso = cursos.filter((curso) => curso.curso == numeroCurso);
+        const pesosCurso = arrayCurso[0].pesos;
 
         for (i = 0; i < elementosPeso.length; i++) {
-            const disciplina = elementosPeso[i].className.slice(5,8);
-            const peso = pesosPrimeiroCurso[disciplina];
-            elementosPeso[i].innerHTML = `<b>${String(peso).replace('.',',')}</b>`;
+          const disciplina = elementosPeso[i].className.slice(5, 8);
+          const peso = pesosCurso[disciplina];
+          elementosPeso[i].innerHTML = `<b>${String(peso).replace('.', ',')}</b>`;
         };
-
-        campusSelect.addEventListener("change", () => {
-            cursoSelect.innerHTML = '';
-
-            cursos.forEach(curso => {
-                if (curso.campus === campusSelect.value) {
-                    atualizarCursos(curso);
-                };
-            });
-
-            atualizarPesos();
-        });
-
-        cursoSelect.addEventListener("change", () => {
-            atualizarPesos();
-        });
-
-        function atualizarCursos(curso) {
-            const option = document.createElement('option');
-            option.value = curso.curso;
-            option.textContent = curso.nome;
-            cursoSelect.appendChild(option);
-        };
-
-        function atualizarPesos() {
-            const numeroCurso = cursoSelect.value;
-            const arrayCurso = cursos.filter((curso) => curso.curso == numeroCurso);
-            const pesosCurso = arrayCurso[0].pesos;
-
-            for (i = 0; i < elementosPeso.length; i++) {
-                const disciplina = elementosPeso[i].className.slice(5,8);
-                const peso = pesosCurso[disciplina];
-                elementosPeso[i].innerHTML = `<b>${String(peso).replace('.',',')}</b>`;
-            };
-        };
+      };
     });
 });
 
@@ -67,34 +67,34 @@ document.addEventListener('DOMContentLoaded', () => {
  */
 
 function NotaFinal() {
-    const notaPli = Number(document.getElementById('pli').value);
-    const notaSli = Number(document.getElementById('sli').value);
-    const notaMtm = Number(document.getElementById('mtm').value);
-    const notaChs = Number(document.getElementById('chs').value);
-    const notaBlg = Number(document.getElementById('blg').value);
-    const notaFsc = Number(document.getElementById('fsc').value);
-    const notaQmc = Number(document.getElementById('qmc').value);
-    const notaDsc = Number(document.getElementById('dsc').value);
-    const notaRdc = Number(document.getElementById('rdc').value);
+  const notaPli = Number(document.getElementById('pli').value);
+  const notaSli = Number(document.getElementById('sli').value);
+  const notaMtm = Number(document.getElementById('mtm').value);
+  const notaChs = Number(document.getElementById('chs').value);
+  const notaBlg = Number(document.getElementById('blg').value);
+  const notaFsc = Number(document.getElementById('fsc').value);
+  const notaQmc = Number(document.getElementById('qmc').value);
+  const notaDsc = Number(document.getElementById('dsc').value);
+  const notaRdc = Number(document.getElementById('rdc').value);
 
-    const numeroCurso = document.getElementById('curso').value;
+  const numeroCurso = document.getElementById('curso').value;
 
-    fetch(cursosPath)
+  fetch(cursosPath)
     .then((response) => response.json())
     .then((json) => {
-        const cursos = json;
+      const cursos = json;
 
-        const arrayCurso = cursos.filter((curso) => curso.curso == numeroCurso);
-        const pesosCurso = arrayCurso[0].pesos;
-        const pmc = Number(arrayCurso[0].pmc);
+      const arrayCurso = cursos.filter((curso) => curso.curso == numeroCurso);
+      const pesosCurso = arrayCurso[0].pesos;
+      const pmc = Number(arrayCurso[0].pmc);
 
-        const pt = (notaPli * pesosCurso.pli) + (notaSli * pesosCurso.sli) + (notaMtm * pesosCurso.mtm) + (notaChs * pesosCurso.chs) + (notaBlg * pesosCurso.blg) + (notaFsc * pesosCurso.fsc) + (notaQmc * pesosCurso.qmc) + (notaDsc * pesosCurso.dsc) + (notaRdc * pesosCurso.rdc);
+      const pt = (notaPli * pesosCurso.pli) + (notaSli * pesosCurso.sli) + (notaMtm * pesosCurso.mtm) + (notaChs * pesosCurso.chs) + (notaBlg * pesosCurso.blg) + (notaFsc * pesosCurso.fsc) + (notaQmc * pesosCurso.qmc) + (notaDsc * pesosCurso.dsc) + (notaRdc * pesosCurso.rdc);
 
-        const nf = (pt * 100)/pmc;
+      const nf = (pt * 100) / pmc;
 
-        const pontuacao = notaPli + notaSli + notaMtm + notaChs + notaBlg + notaFsc + notaQmc + notaDsc + notaRdc;
-    
-        document.getElementById('notafinal').innerHTML = `<b>Nota final:</b> ${nf.toFixed(2).replace('.', ',')}`;
-        document.getElementById('pontuacaototal').innerHTML = `<b>Pontuação Total:</b> ${pontuacao.toFixed(2).replace('.', ',')}`;
+      const pontuacao = notaPli + notaSli + notaMtm + notaChs + notaBlg + notaFsc + notaQmc + notaDsc + notaRdc;
+
+      document.getElementById('notafinal').innerHTML = `<b>Nota final:</b> ${nf.toFixed(2).replace('.', ',')}`;
+      document.getElementById('pontuacaototal').innerHTML = `<b>Pontuação Total:</b> ${pontuacao.toFixed(2).replace('.', ',')}`;
     });
 };
--- js/somatoria.js
+++ js/somatoria.js
@@ -6,57 +6,57 @@
  */
 
 function Somatoria() {
-    const np = Number(document.getElementById("quantidade").value);
-    let soma = Number(document.getElementById("soma").value);
-    let gabarito = Number(document.getElementById("gabarito").value);
+  const np = Number(document.getElementById("quantidade").value);
+  let soma = Number(document.getElementById("soma").value);
+  let gabarito = Number(document.getElementById("gabarito").value);
 
-    if (np < 3 | np > 7 | soma <= 0 | soma > 99 | gabarito <= 0 | gabarito > 99) {
-        return alert("Há algum dado incorreto!");
-    };
+  if (np < 3 | np > 7 | soma <= 0 | soma > 99 | gabarito <= 0 | gabarito > 99) {
+    return alert("Há algum dado incorreto!");
+  };
 
-    if (soma > (2 ** np) - 1 | gabarito > (2 ** np) - 1) {
-        return alert("A soma ou o gabarito estão acima do máximo!");
-    };
+  if (soma > (2 ** np) - 1 | gabarito > (2 ** np) - 1) {
+    return alert("A soma ou o gabarito estão acima do máximo!");
+  };
 
-    let preps = [];
+  let props = [];
 
-    for (let i = np - 1; i >= 0; i--) {
-        preps.push(2**i);
-    };
+  for (let i = np - 1; i >= 0; i--) {
+    props.push(2 ** i);
+  };
 
-    let ntpc = 0;
-    let corretas = [];
-    let incorretas = [];
-    let assinaladas = [];
+  let ntpc = 0;
+  let corretas = [];
+  let incorretas = [];
+  let assinaladas = [];
 
-    preps.forEach((prep) => {
-        if (gabarito >= prep) {
-            ntpc++;
-            corretas.push(prep);
-            gabarito -= prep;
-        } else {
-            incorretas.push(prep);
-        };
-
-        if (soma >= prep) {
-            assinaladas.push(prep);
-            soma -= prep;
-        };
-    });
-
-    const npc = corretas.filter(i => assinaladas.includes(i)).length;
-    const npi = incorretas.filter(i => assinaladas.includes(i)).length;
-
-    let p;
-
-    if (npc > npi) {
-        p = (np - (ntpc - (npc - npi))) / np;
+  props.forEach((prep) => {
+    if (gabarito >= prep) {
+      ntpc++;
+      corretas.push(prep);
+      gabarito -= prep;
     } else {
-        p = 0;
+      incorretas.push(prep);
     };
 
-    document.getElementById('pontuacao').innerHTML = `<b>Pontuação:</b> ${p.toFixed(2).replace('.', ',')}`;
-    document.getElementById('corretas').innerHTML = `<b>Corretas:</b> ${corretas.join(',&thinsp;')}`;
-    document.getElementById('incorretas').innerHTML = `<b>Incorretas:</b> ${incorretas.join(',&thinsp;')}`;
-    document.getElementById('assinaladas').innerHTML = `<b>Assinaladas:</b> ${assinaladas.join(',&thinsp;')}`;
+    if (soma >= prep) {
+      assinaladas.push(prep);
+      soma -= prep;
+    };
+  });
+
+  const npc = corretas.filter(i => assinaladas.includes(i)).length;
+  const npi = incorretas.filter(i => assinaladas.includes(i)).length;
+
+  let p;
+
+  if (npc > npi) {
+    p = (np - (ntpc - (npc - npi))) / np;
+  } else {
+    p = 0;
+  };
+
+  document.getElementById('pontuacao').innerHTML = `<b>Pontuação:</b> ${p.toFixed(2).replace('.', ',')}`;
+  document.getElementById('corretas').innerHTML = `<b>Corretas:</b> ${corretas.join(',&thinsp;')}`;
+  document.getElementById('incorretas').innerHTML = `<b>Incorretas:</b> ${incorretas.join(',&thinsp;')}`;
+  document.getElementById('assinaladas').innerHTML = `<b>Assinaladas:</b> ${assinaladas.join(',&thinsp;')}`;
 };
