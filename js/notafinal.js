72f4fe90d05b44ab96b76519dc728d8982154756
Mateus Bertan <contato@mateusbertan.com>
2025-11-20 20:02:30 -0300

    Inputs de decimal melhorados

--- js/notafinal.js
+++ js/notafinal.js
@@ -67,15 +67,15 @@ document.addEventListener('DOMContentLoaded', () => {
  */
 
 function NotaFinal() {
-  const notaPli = Number(document.getElementById('pli').value);
-  const notaSli = Number(document.getElementById('sli').value);
-  const notaMtm = Number(document.getElementById('mtm').value);
-  const notaChs = Number(document.getElementById('chs').value);
-  const notaBlg = Number(document.getElementById('blg').value);
-  const notaFsc = Number(document.getElementById('fsc').value);
-  const notaQmc = Number(document.getElementById('qmc').value);
-  const notaDsc = Number(document.getElementById('dsc').value);
-  const notaRdc = Number(document.getElementById('rdc').value);
+  const notaPli = Number(document.getElementById('pli').value.replace(/\D/g, "")/100);
+  const notaSli = Number(document.getElementById('sli').value.replace(/\D/g, "")/100);
+  const notaMtm = Number(document.getElementById('mtm').value.replace(/\D/g, "")/100);
+  const notaChs = Number(document.getElementById('chs').value.replace(/\D/g, "")/100);
+  const notaBlg = Number(document.getElementById('blg').value.replace(/\D/g, "")/100);
+  const notaFsc = Number(document.getElementById('fsc').value.replace(/\D/g, "")/100);
+  const notaQmc = Number(document.getElementById('qmc').value.replace(/\D/g, "")/100);
+  const notaDsc = Number(document.getElementById('dsc').value.replace(/\D/g, "")/100);
+  const notaRdc = Number(document.getElementById('rdc').value.replace(/\D/g, "")/100);
 
   const numeroCurso = document.getElementById('curso').value;
 
@@ -88,7 +88,16 @@ function NotaFinal() {
       const pesosCurso = arrayCurso[0].pesos;
       const pmc = Number(arrayCurso[0].pmc);
 
-      const pt = (notaPli * pesosCurso.pli) + (notaSli * pesosCurso.sli) + (notaMtm * pesosCurso.mtm) + (notaChs * pesosCurso.chs) + (notaBlg * pesosCurso.blg) + (notaFsc * pesosCurso.fsc) + (notaQmc * pesosCurso.qmc) + (notaDsc * pesosCurso.dsc) + (notaRdc * pesosCurso.rdc);
+      const pt =
+        (notaPli * pesosCurso.pli) +
+        (notaSli * pesosCurso.sli) +
+        (notaMtm * pesosCurso.mtm) +
+        (notaChs * pesosCurso.chs) +
+        (notaBlg * pesosCurso.blg) +
+        (notaFsc * pesosCurso.fsc) +
+        (notaQmc * pesosCurso.qmc) +
+        (notaDsc * pesosCurso.dsc) +
+        (notaRdc * pesosCurso.rdc);
 
       const nf = (pt * 100) / pmc;
 
